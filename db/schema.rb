eebe81347262976e47b474c043a5a0df2b6f3375
Lukáš Doležal <dolezal@itlab.cz>
2013-04-25 11:03:28 -0600

    Forgoten file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419221304) do
+ActiveRecord::Schema.define(:version => 20130425155325) do
 
   create_table "activities", :force => true do |t|
     t.string   "name",        :null => false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20130419221304) do
     t.datetime "start",       :null => false
     t.integer  "duration",    :null => false
     t.integer  "activity_id"
+    t.datetime "end"
   end
 
 end
