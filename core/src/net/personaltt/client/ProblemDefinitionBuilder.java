dba9e55d141a93fea656cc83e40164ae7f336482
Lukáš Doležal <dolezal@itlab.cz>
2013-06-08 20:35:00 +0200

    CORE - renamed bad package name

--- core/src/net/personaltt/client/ProblemDefinitionBuilder.java
+++ core/src/net/personaltt/client/ProblemDefinitionBuilder.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package client;
+package net.personaltt.client;
 
 import net.personaltt.problem.Occurrence;
 import net.personaltt.problem.ProblemDefinition;
--- core/src/net/personaltt/client/ScheduleParser.java
+++ core/src/net/personaltt/client/ScheduleParser.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package client;
+package net.personaltt.client;
 
 import net.personaltt.problem.OccurrenceAllocation;
 import net.personaltt.problem.Schedule;
